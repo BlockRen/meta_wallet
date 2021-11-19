235c65e871e100a1cd29f890fd23dd00ac104cfc
aaron <92631595+BlockRen@users.noreply.github.com>
2021-11-19 22:09:48 +0800

    Avatar page added control button.
    The avatar animation can be control through the float button.

--- lib/level_3_business/avatar/avatar_page.dart
+++ lib/level_3_business/avatar/avatar_page.dart
@@ -1,8 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:rive/rive.dart';
 
-class AvatarAnimation extends StatelessWidget {
+class AvatarAnimation extends StatefulWidget {
   const AvatarAnimation({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _AvatarAnimationState();
+}
+
+class _AvatarAnimationState extends State<AvatarAnimation> {
+  // Controller for playback
+  late RiveAnimationController _controller;
+
+  // Toggles between play and pause animation states
+  void _togglePlay() => setState(() {
+    _controller.isActive = !_controller.isActive;
+  });
+
+  /// Tracks if the animation is playing by whether controller is running
+  bool get isPlaying => _controller.isActive;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = SimpleAnimation('Rage');
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,12 +32,23 @@ class AvatarAnimation extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Avatar"),
       ),
-      body: const Center(
+      body: Center(
         child: RiveAnimation.asset(
           'assets/tom_morello.riv',
           fit: BoxFit.fitWidth,
           artboard: 'Artboard',
-          animations: ['Idle', 'Rage'],
+          controllers: [_controller],
+          onInit: (_) => setState(() {
+            _controller.isActive = false;
+          }),
+          // animations: ['Idle', 'Rage'],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _togglePlay,
+        tooltip: isPlaying ? 'Pause' : 'Play',
+        child: Icon(
+          isPlaying ? Icons.pause : Icons.play_arrow,
         ),
       ),
     );
