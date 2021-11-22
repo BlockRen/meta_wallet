e6f600a69d80f0aa77d9bd11a6cb605e4e13f8e3
aaron <92631595+BlockRen@users.noreply.github.com>
2021-11-22 22:14:08 +0800

    Update the transaction fetch class to extend the FetchBase class.

--- lib/level_3_business/transaction/transaction_fetch.dart
+++ lib/level_3_business/transaction/transaction_fetch.dart
@@ -1,40 +1,43 @@
+import 'package:meta_wallet/level_1_core/network/fetch_base.dart';
 import 'package:meta_wallet/level_1_core/network/http_request.dart';
 import 'package:meta_wallet/level_1_core/storage/file_storage.dart';
 import 'package:meta_wallet/level_1_core/util/magic_value.dart';
 import 'package:meta_wallet/level_2_ui/model/transaction_model.dart';
 
-class TransactionFetch {
+class TransactionFetch extends FetchBase {
   TransactionFetch();
 
-  // update the data after one minute
-  static const int _updateScope = 60000;
-  // record the last update time
-  static int _lastUpdateTime = DateTime.now().millisecondsSinceEpoch - _updateScope;
+  @override
+  Future<Object> requestOnlineData(RefreshType refreshType, int pageIndex) async {
+    List transactionData = await HttpRequest().jsonRequest(MagicValue.transactionInfoUrl) ?? [];
+    return transactionData;
+  }
+
+  /// covert online data to model.
+  List _convertData(Object? onlineData) {
+    if (onlineData == null) {
+      return [];
+    }
+    // model
+    List transactionModels = [];
+    List iterableOnlineData = onlineData as List;
+    // Filter out lack of financial data
+    for (Map data in iterableOnlineData) {
+      TransactionModel model = TransactionModel(data);
+      transactionModels.add(model);
+    }
+    return transactionModels;
+  }
 
   Future<void> loadTransactionInfo(Function onLoadFinish) async {
-    // Json data
-    List _transactionData = [];
-    // model
-    List _transactionModels = [];
-
-    bool fileExist = await fileStorage.exists(gStoreTransaction);
-    if (fileExist &&
-        (_lastUpdateTime - DateTime.now().millisecondsSinceEpoch).abs() < _updateScope) {
-      List jsonObject = await fileStorage.readJson(gStoreTransaction) as List;
-      _transactionData.addAll(jsonObject);
-    } else {
-      _transactionData = await HttpRequest().jsonRequest(MagicValue.transactionInfoUrl) ?? [];
-      /// save to local file.
-      fileStorage.writeJson(_transactionData, gStoreTransaction);
-      // reset time
-      _lastUpdateTime = DateTime.now().millisecondsSinceEpoch;
+    /// first show list with local file data to accelerate the show.
+    Object? data = await fetchFileData(gStoreTransaction);
+    if (data != null) {
+      onLoadFinish(_convertData(data));
     }
-
-    // Filter out lack of financial data
-    for (Map data in _transactionData) {
-      TransactionModel model = TransactionModel(data);
-      _transactionModels.add(model);
-    }
-    onLoadFinish(_transactionModels);
+    /// call super method to fetch data online.
+    fetchOnlineData((Object? data, bool isOnline) {
+      onLoadFinish(_convertData(data));
+    }, fileName: gStoreTransaction);
   }
 }
