6d7904daabcc2fa5e98c7dfef985464fb8319cd3
aaron <92631595+BlockRen@users.noreply.github.com>
2021-11-22 22:12:30 +0800

    Provide timeout and bad certificate process for network class.

--- lib/level_1_core/network/http_request.dart
+++ lib/level_1_core/network/http_request.dart
@@ -1,14 +1,23 @@
 import 'dart:convert';
 import 'dart:io';
+import 'package:dio/adapter.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:dio/dio.dart';
 
 class HttpRequest<T> {
-  HttpRequest();
+  final Dio _dio = Dio();
+
+  HttpRequest() {
+    (_dio.httpClientAdapter as DefaultHttpClientAdapter).onHttpClientCreate = (client){
+      client.badCertificateCallback=(cert, host, port){
+        return true;
+      };
+    };
+  }
 
   Future<T?> jsonRequest(String urlString) async {
     try {
-      var response = await Dio().get(urlString, options: Options(sendTimeout: 5000));
+      var response = await _dio.get(urlString, options: Options(sendTimeout: 5000));
       if (response.statusCode == HttpStatus.ok) {
         return response.data;
       }
