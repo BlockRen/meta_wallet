10144a6aaf5214e0ece8f8c7745e53a22cab5442
aaron <92631595+BlockRen@users.noreply.github.com>
2022-01-17 21:42:37 +0800

    Upload the forest tiled map.

---
+++ assets/tiles/tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="tileset" tilewidth="32" tileheight="32" tilecount="324" columns="18">
+ <image source="../images/tileset.png" trans="ff00ff" width="595" height="595"/>
+</tileset>
